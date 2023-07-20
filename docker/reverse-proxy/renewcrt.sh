027545ad015272dacd63663a2574e67c41377e05
VoyNaLunu <lalalalaskir@gmail.com>
2023-07-20 13:21:44 +0300

    renewcrt script now gracefully reloads nginx

--- docker/reverse-proxy/renewcrt.sh
+++ docker/reverse-proxy/renewcrt.sh
@@ -4,6 +4,5 @@
 docker compose run --rm certbot renew | grep 'No renewals were attempted'
 if [ $? -ne 0 ]
 then
-    nginx=$(docker ps -qaf name=reverse-proxy-nginx)
-    docker restart ${nginx}
+    docker compose exec nginx nginx -s reload
 fi
