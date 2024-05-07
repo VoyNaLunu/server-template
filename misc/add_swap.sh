c090f93ed296a9df7aa2b87773a2c2cf20ba7ecf
VoyNaLunu <93346826+VoyNaLunu@users.noreply.github.com>
2024-05-07 13:31:06 +0300

    improved add_swap.sh

--- misc/add_swap.sh
+++ misc/add_swap.sh
@@ -1,12 +1,49 @@
 #!/bin/bash
-#Usage: add_swap.sh /path/to/swapfile 2G
-if [ -f $1 ]
-then
-    echo "swap file $1 already exists" && exit 0
+
+help=false
+size=1GB
+no_args=true
+number_re='^[1-9]+(KB|MB|GB|TB|PB|ZB|YB)$'
+
+
+while getopts hp:s: flag
+do
+    case "${flag}" in
+        p) path=${OPTARG};;
+        s) size=${OPTARG};;
+        h) help=true;;
+        *) help=true;;
+    esac
+    no_args=false
+done
+
+if [ $no_args == true ] || [ $help == true ]
+then 
+    echo "Usage: ./add_swap.sh [-p <path>][-s <size><KB|MB|GB|TB|PB|ZB|YB>][-h]
+    Options:
+        -h      This help message
+        -p      Path to the swapfile
+        -s      Size of swapfile in KB|MB|GB|TB|PB|ZB|YB, defaults to 1GB if no value provided
+    " && exit 0
 fi
 
-fallocate -l $2 $1
-chmod 600 $1
-mkswap $1
-swapon $1
-echo "$1 swap swap defaults 0 0" >> /etc/fstab
+if [ -z ${path:+"x"} ]
+then
+    echo "No path was provided" && exit 1
+fi
+
+if [ -f $path ]
+then
+    echo "Swap file $path already exists" && exit 1
+fi
+
+if ! [[ $size =~ $number_re ]]
+then
+    echo "Size must be a positive integer and have a size unit, example: 2GB" && exit 1
+fi
+
+fallocate -l "${size}" $path
+chmod 600 $1path
+mkswap $path
+swapon $path
+echo "$path swap swap defaults 0 0" >> /etc/fstab
